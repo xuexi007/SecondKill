b1920d2e90133e9171e8597e2254e652a1e8a453
葱花鸡蛋 <553899811@qq.com>
2018-06-24 07:57:09 +0800

    SecondKill项目初始化

---
+++ src/main/java/com/zhangyong/SecondKillApplication.java
@@ -0,0 +1,20 @@
+package com.zhangyong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>ClassName:SecondKillApplication</p>
+ * <p>Description:	</p>
+ * <p>Company: www.shopin.net</p>
+ *
+ * @author zhangyong@shopin.cn
+ * @version 1.0
+ * @date 2018/6/23 17:58
+ */
+@SpringBootApplication
+public class SecondKillApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecondKillApplication.class, args);
+    }
+}
---
+++ src/main/java/com/zhangyong/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.zhangyong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("name", "Echizen");
+        return "hello";
+    }
+
+}
